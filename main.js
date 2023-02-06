5b22881304542032c899ad88e6f9d4f4f44792e8
Ranjith4024 <kumar.ranjith.srk@gmail.com>
2023-02-06 16:36:34 +0530

    adding

---
+++ main.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    first_name: 'Mike',
+    last_name: 'Sheridan'
+  },
+  {
+    first_name: 'Tim',
+    last_name: 'Lee'
+  },
+  {
+    first_name: 'John',
+    last_name: 'Carte'
+  }
+];
+
+// output =  ['Mike Sheridan', 'Tim Lee', 'John Carte']
+
+const fullName = users.map(function(val){
+     return `${val.first_name} ${val.last_name}`;
+	 });
+	 
+	 console.log(fullName);
